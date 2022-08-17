3ccd4c852760d3a6b3cb8588f073e59078c37537
ilin-ei <ilin.ei@yandex.ru>
2022-08-17 22:04:39 +0300

    Transactional on class and auto-set localDate

--- src/main/java/org/aston/service/impl/LogServiceImpl.java
+++ src/main/java/org/aston/service/impl/LogServiceImpl.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
+@Transactional
 public class LogServiceImpl implements LogService {
 
     private final LogRepository logRepository;
@@ -20,31 +22,27 @@ public class LogServiceImpl implements LogService {
     }
 
     @Override
-    @Transactional
     public Log getLog(Long id) {
         return logRepository.getById(id);
     }
 
     @Override
-    @Transactional
     public List<Log> getLogsByStudentId(Long studentId) {
         return logRepository.getAllByStudentId(studentId);
     }
 
     @Override
-    @Transactional
     public Log addLog(Log log) {
+    	log.setDate(LocalDate.now());
         return logRepository.save(log);
     }
 
     @Override
-    @Transactional
     public Log updateLog(Log log) {
         return logRepository.update(log);
     }
 
     @Override
-    @Transactional
     public Log deleteLog(Long id) {
         return logRepository.deleteById(id);
     }
