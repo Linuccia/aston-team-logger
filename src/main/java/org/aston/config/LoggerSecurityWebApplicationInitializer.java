0f93dad7d487918f3edea48e4318815e59dbf75f
rst <rst5995@gmail.com>
2022-08-18 14:11:35 +0300

    Add basic security

---
+++ src/main/java/org/aston/config/LoggerSecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package org.aston.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class LoggerSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
